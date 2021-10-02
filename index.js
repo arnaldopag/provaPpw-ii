bde0134f0d0b3c0a1d6ac748c67cbc7329c3c2c2
arnaldopag <arnaldopaganijr@gmail.com>
2021-10-01 21:44:32 -0300

    Adicionado README.MD

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = require("./routes");
 
 const app = express();
-const port = 3000;
-//process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const cors = require("cors");
 
